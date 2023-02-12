092bcdc086c072b871e7b0c5199623ad76a05916
sw <sw@email.com>
2023-02-12 11:12:27 +0100

    starrt

---
+++ ArkuaszPraca/Program.cs
@@ -0,0 +1,39 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkuaszPraca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zaczynamy");
+            string[] dni = { "po" ,"wto","śro","czw","pia","so","nd"};
+            var date = DateTime.Now;
+            var lastDayOfMonth = DateTime.DaysInMonth(date.Year, date.Month);
+            int start = 5;
+       
+            using (ExcelPackage eksel=new ExcelPackage())
+            {
+                var ff = eksel.Workbook.Worksheets.Add("start");
+                for (int i = 1; i <=lastDayOfMonth; i++)
+                {
+                    int pos = i % 7;
+                    DateTime ds = new DateTime(date.Year, date.Month, i);
+                    ff.Cells[start+i, 2].Value = i;
+                    int day = ((int)ds.DayOfWeek == 0) ? 7 : (int)ds.DayOfWeek;
+                    day--;
+                    ff.Cells[start+i, 3].Value = dni[day];
+
+
+                }
+                ff.Cells["D"+(lastDayOfMonth+start+1).ToString()].Formula = "=SUM(D"+(start+1).ToString()+":D"+(lastDayOfMonth+start).ToString()+")";
+                eksel.SaveAs(new System.IO.FileInfo("aa.xlsx"));
+            }
+        }
+    }
+}
